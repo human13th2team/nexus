1e48a844f5ec9034154078f3728ae45a25f65a12
ashfortune <yjb1521@naver.com>
2026-04-27 16:26:24 +0900

    fix:Header, Footer 중복 파일 수정

--- frontend-next/app/layout.tsx
+++ frontend-next/app/layout.tsx
@@ -1,8 +1,7 @@
 import type { Metadata } from "next";
 import { Manrope, Inter } from "next/font/google";
 import "./globals.css";
-import Header from "@/components/Header";
-import Footer from "@/components/Footer";
+
 
 const manrope = Manrope({
   variable: "--font-manrope",
